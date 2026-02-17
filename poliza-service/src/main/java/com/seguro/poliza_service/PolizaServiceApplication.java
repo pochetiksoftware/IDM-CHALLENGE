43b4e09ccb8b844b8470798ad6e16148d7ac1c99
Pool Gabrielomar Meneses Llacza <pmenesel@emeal.nttdata.com>
2026-02-17 16:25:50 -0500

    init insurance microservices

---
+++ poliza-service/src/main/java/com/seguro/poliza_service/PolizaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.seguro.poliza_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolizaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolizaServiceApplication.class, args);
+	}
+
+}
---
+++ poliza-service/src/test/java/com/seguro/poliza_service/PolizaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seguro.poliza_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolizaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
